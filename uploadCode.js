c2fe550754d0d1b38c2c9758a7869214d7e488a6
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-08 11:14:29 -0700

    removing text from .env

--- uploadCode.js
+++ uploadCode.js
@@ -3,5 +3,3 @@ DB_URL = "mongodb+srv://jedihomeslice:Tenaya9!@cluster0.dlh4wdv.mongodb.net/Thre
 JWT_SECRET_KEY = "secret6942069"
 NodeMailer_password = "jumkiyiaeqpjnrla"
 NodeMailer_email = "jedihomeslice@gmail.com"
-
-// this is some text from Eric
