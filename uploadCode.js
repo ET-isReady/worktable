5ce2a1cd8a93bf5a45ac37109082a4fac8be9713
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-08 11:08:27 -0700

    Add the .env code

---
+++ uploadCode.js
@@ -0,0 +1,5 @@
+PORT = 3000
+DB_URL = "mongodb+srv://jedihomeslice:Tenaya9!@cluster0.dlh4wdv.mongodb.net/Threads?retryWrites=true&w=majority"
+JWT_SECRET_KEY = "secret6942069"
+NodeMailer_password = "jumkiyiaeqpjnrla"
+NodeMailer_email = "jedihomeslice@gmail.com"
