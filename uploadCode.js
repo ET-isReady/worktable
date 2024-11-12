7f98104536fef118c3ea105139075415b0f74517
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-12 14:28:26 -0700

    add Account Created file

--- uploadCode.js
+++ uploadCode.js
@@ -1,9 +1,54 @@
-PORT = 3000
-DB_URL = "mongodb+srv://jedihomeslice:Tenaya9!@cluster0.dlh4wdv.mongodb.net/Threads?retryWrites=true&w=majority"
-JWT_SECRET_KEY = "secret6942069"
-NodeMailer_password = "jumkiyiaeqpjnrla"
-NodeMailer_email = "jedihomeslice@gmail.com"
+import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, TouchableOpacity } from 'react-native'
+import React from 'react'
+import logo from '../../../../assets/pp_logo.png'
+import greenBg from '../../../../assets/nft01.png'
+import { containerFull, logo1, greenBackground,
+brandView, brandViewText, bottomView, goback, row } from '../../../CommonCss/pagecss'
+import { formTop, formHead2, formbtn2 } from '../../../CommonCss/formcss'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-    })
+
+const Signup_AccountCreated = ({ navigation, route })=>{
+return (
+
+   <ScrollView style={containerFull}>
+  <ImageBackground source={greenBg} style={greenBackground}>
+  <View>
+  <TouchableOpacity style={goback} onPress={()=> navigation.navigate('Login')}>
+  <Ionicons name="arrow-undo-circle" size={40} color="white" />
+  </TouchableOpacity>
+  <Text style={{ fontSize: 16, fontWeight: 'bold', color: 'white', marginVertical: 40, marginLeft: 65,
+  position: 'absolute'}}>Go back</Text>
+  </View>
+  <View style={brandView}>
+  <Image style={logo1} source={logo}/>
+  <Text style={brandViewText}>Poetionpics</Text>
+  </View>
+  </ImageBackground>
+
+  <View style={bottomView}>
+  <View style={formTop}>
+
+
+    <View style={row}>
+    <Text style={formHead2}>Account Created Successfully! </Text>
+      <MaterialCommunityIcons name="check-decagram" size={30} color="#7eff00" />
+    </View>
+    <Text style={formbtn2}
+    onPress={()=> navigation.navigate('Login')}>
+      Let's do this!
+    </Text>
+  </View>
+  </View>
+  </ScrollView>
+)
+}
+
+
+export default Signup_AccountCreated
+
+
+const styles = StyleSheet.create({
+
+})
