64cfb47f3e4abf4b862c7cb4908bb2e45890fb7c
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-12 15:31:50 -0700

    add FP Enter email code

--- uploadCode.js
+++ uploadCode.js
@@ -1,53 +1,48 @@
-import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, Alert, TouchableOpacity,
-    ActivityIndicator } from 'react-native'
+import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, TouchableOpacity,
+    ActivityIndicator, Alert } from 'react-native'
     import React, { useState } from 'react'
     import logo from '../../../../assets/pp_logo.png'
-    import greenBg from '../../../../assets/nft06.jpeg'
+    import greenBg from '../../../../assets/purple_tech.jpeg'
     import { containerFull, logo1, greenBackground,
-      brandView, brandViewText, bottomView, goback, row } from '../../../CommonCss/pagecss'
+      brandView, brandViewText, bottomView, goback } from '../../../CommonCss/pagecss'
+    import { formTop, formInput, formbtn, formHead5 } from '../../../CommonCss/formcss'
+    import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
     import Ionicons from 'react-native-vector-icons/Ionicons'
-    import { formTop, formInput, formbtn, formHead4, } from '../../../CommonCss/formcss'
- 
-     
-      const ForgotPassword_ChoosePassword = ({navigation, route}) => {
+
+
+    const ForgotPassword_EnterEmail = ({ navigation })=>{
   
-      const { email } = route.params
+      const [email, setEmail] = useState('')
+      const [loading, setLoading] = useState(false)
   
-      const [ password, setPassword ] = useState('')
-      const [ confirmPassword, setConfirmPassword ] = useState('')
-      const [ loading, setLoading ] = useState(false)
-  
-      const handleChangePassword = ()=>{
-        if( password == '' || confirmPassword == ''){
-          Alert.alert('Please enter password')
-        }else if( password != confirmPassword ){
-          Alert.alert('Passwords do not match!')
+      const handleEmail = ()=>{
+        if(email === ''){
+          Alert.alert('Please Enter Your Email')
         } else{
           setLoading(true)
-          fetch('http://10.0.2.2:3000/resetpassword', {
+          fetch('http://10.0.2.2:3000/verifyfp', {
             method: 'post',
             headers: {
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ email: email, password: password })
+            body: JSON.stringify({ email: email })
           })
-          .then( res => res.json()).then(
-            data => {
-              if( data.message === "Password Changed Successfully!"){
-                setLoading(false)
-                Alert.alert(data.message)
-                navigation.navigate('ForgotPassword_AccountRecovered')
-              } else{
-                setLoading(false)
-                Alert.alert("Oops! Something went wrong. Please try again.")
-              }
-            })
-          .catch(err=>{
+          .then(res => res.json()).then(data => {
+            if(data.error === "Invalid Credentials"){
+            //alert('Invalid Credentials')
+            Alert.alert('Invalid Credentials')
             setLoading(false)
-            Alert.alert(err)
+          }
+          else if (data.message === "Verification Code Sent to your Email") {
+            setLoading(false)
+            Alert.alert(data.message);
+            navigation.navigate('ForgotPassword_EnterVerificationCode', {
+              useremail: data.email,
+              userVerificationCode : data.VerificationCode
+            })
+          }
           })
         }
-  
       }
   
       return (
@@ -70,24 +65,18 @@ import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, 
         <View style={bottomView}>
         <View style={formTop}>
   
-          <Text style={formHead4}>Choose A New Password</Text>
+          <Text style={formHead5}>Please Verify Your Email</Text>
   
           <View style={{ marginTop: 50 }}>
-            <TextInput placeholder='Enter Password' style={formInput}
-            onChangeText={(text)=>setPassword(text)}
-            />
-          </View>
-          <View style={{ marginTop: 50 }}>
-            <TextInput placeholder='Confirm Password' style={formInput}
-            onChangeText={(text)=>setConfirmPassword(text)}
+            <TextInput keyboardType='email-address' placeholder='Enter Your Email' style={formInput}
+            onChangeText={(text)=>setEmail(text)}
             />
           </View>
           {
             loading ?
-            <ActivityIndicator size="large" color='#00FF7F' />
-            :
+            <ActivityIndicator size="large" color='#00FF7F' /> :
             <Text style={formbtn}
-              onPress={()=> handleChangePassword()}>
+              onPress={()=> handleEmail()}>
               Next
             </Text>
           }
@@ -97,9 +86,7 @@ import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, 
       )
     }
   
-  
-  
-    export default ForgotPassword_ChoosePassword
+    export default ForgotPassword_EnterEmail
   
     const styles = StyleSheet.create({
   
