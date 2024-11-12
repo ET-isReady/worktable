6819a775ed14408de810c432c79d3b4da433bbc0
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-12 14:32:29 -0700

    adding pagecss file

--- uploadCode.js
+++ uploadCode.js
@@ -1,54 +1,243 @@
-import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, TouchableOpacity } from 'react-native'
-import React from 'react'
-import logo from '../../../../assets/pp_logo.png'
-import greenBg from '../../../../assets/nft01.png'
-import { containerFull, logo1, greenBackground,
-brandView, brandViewText, bottomView, goback, row } from '../../../CommonCss/pagecss'
-import { formTop, formHead2, formbtn2 } from '../../../CommonCss/formcss'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import { Dimensions } from 'react-native'
 
 
-const Signup_AccountCreated = ({ navigation, route })=>{
-return (
-
-   <ScrollView style={containerFull}>
-  <ImageBackground source={greenBg} style={greenBackground}>
-  <View>
-  <TouchableOpacity style={goback} onPress={()=> navigation.navigate('Login')}>
-  <Ionicons name="arrow-undo-circle" size={40} color="white" />
-  </TouchableOpacity>
-  <Text style={{ fontSize: 16, fontWeight: 'bold', color: 'white', marginVertical: 40, marginLeft: 65,
-  position: 'absolute'}}>Go back</Text>
-  </View>
-  <View style={brandView}>
-  <Image style={logo1} source={logo}/>
-  <Text style={brandViewText}>Poetionpics</Text>
-  </View>
-  </ImageBackground>
-
-  <View style={bottomView}>
-  <View style={formTop}>
-
-
-    <View style={row}>
-    <Text style={formHead2}>Account Created Successfully! </Text>
-      <MaterialCommunityIcons name="check-decagram" size={30} color="#7eff00" />
-    </View>
-    <Text style={formbtn2}
-    onPress={()=> navigation.navigate('Login')}>
-      Let's do this!
-    </Text>
-  </View>
-  </View>
-  </ScrollView>
-)
+module.exports = {
+ newThread: {
+   width: '100%',
+   flexDirection: 'row',
+   alignItems: 'center',
+   padding: 5
+ },
+ threadText: {
+   fontSize: 20,
+   marginLeft: 10
+ },
+ tinyProfPic: {
+   width: 30,
+   height: 30,
+   borderRadius: 44 / 2,
+   borderColor: '#00FF7F',
+   borderWidth: 1
+ },
+ postUsername: {
+   marginLeft: 10,
+   fontSize: 15,
+   fontWeight: 'bold',
+   color: '#000'
+ },
+ postNft_image: {
+   width: '100%',
+   height: 300,
+   borderRadius: 10,
+ },
+ userLists: {
+   width: '100%',
+   marginTop: 20
+ },
+ searchUsersCard: {
+   width: '97.5%',
+   height: 75,
+   backgroundColor: 'white',
+   borderRadius: 10,
+   marginVertical: 3,
+   flexDirection: 'row',
+   marginLeft: 5
+ },
+ searchProfilePic: {
+   width: 50,
+   height: 50,
+   borderRadius: 50,
+   borderColor: 'red',
+   borderWidth: 1,
+   marginTop: 5
+ },
+ searchView: {
+   marginLeft: 10
+ },
+ searchUsername: {
+   color: 'black',
+   fontSize: 20
+},
+searchUserheader: {
+ fontSize: 10,
+ paddingRight: 49
+},
+searchFans: {
+ fontSize: 10,
+ paddingTop: 2,
+ color: '#0c6df5'
+},
+containerFull: {
+  flex: 1,
+  backgroundColor: '#ffffff'
+},
+mainpageFull: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center'
+},
+greenBackground: {
+  height: Dimensions.get('window').height / 2.5
+},
+brandView: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center'
+},
+brandViewText: {
+  color: '#ffffff',
+  fontSize: 40,
+  textTransform: 'lowercase'
+},
+logo1: {
+  height: 100,
+  resizeMode: 'contain'
+},
+bottomView: {
+  flex: 1.5,
+  backgroundColor: '#ffffff',
+  bottom: 50,
+  borderTopStartRadius: 60,
+  borderTopEndRadius: 60
+},
+goback: {
+  flexDirection: 'row',
+  position: 'absolute',
+  top: 30,
+  left: 20,
+  alignItems: 'center'
+},
+row: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginVertical: 10
+},
+logo2: {
+  width: 100,
+  height: 30,
+  resizeMode: 'contain',
+  marginLeft: -15
+},
+icons1: {
+  color: 'white',
+  fontSize: 35,
+},
+searchBar: {
+  width: '90%',
+  backgroundColor: 'white',
+  borderRadius: 30,
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  marginTop: 10,
+  fontSize: 18,
+  alignSelf: 'center'
+},
+noteCard: {
+ backgroundColor: 'white',
+ width: '100%',
+ marginTop: 10,
+ borderRadius: 20,
+ padding: 10,
+ flexDirection: 'row',
+ borderRadius: 20,
+ borderColor: '#B700B7',
+ borderWidth: .5,
+},
+noteProfileImage: {
+ width: 50,
+ height: 50,
+ borderRadius: 50,
+ borderColor: 'red',
+ borderWidth: 1
+},
+noteC1: {
+ marginLeft: 10
+},
+noteUsername: {
+ fontSize: 15,
+},
+noteLastMessage: {
+ fontSize: 19
+},
+my_img: {
+  width: 33,
+  height: 33,
+  resizeMode: 'contain',
+  borderRadius: 400/2,
+  borderColor: '#00FF7F',
+  borderWidth: 1
+},
+c1: {
+ width: '100%',
+ alignItems: 'center'
+},
+profilePic: {
+ width: 150,
+ height: 150,
+ borderRadius: 400/2,
+ borderColor: '#00FF7F',
+ borderWidth: 1,
+ margin: 20
+},
+txt: {
+ color: 'black',
+ //backgroundColor: 'green',
+ fontSize: 20,
+ fontWeight: 'bold',
+ marginTop: -15,
+ marginBottom: 10,
+ paddingVertical: 10,
+ paddingHorizontal: 20,
+ borderRadius: 20
+},
+txt1: {
+ color: 'green',
+ fontSize: 15,
+},
+txt2: {
+ color: 'green',
+ fontSize: 20
+},
+c11: {
+ width: '100%',
+ flexDirection: 'row',
+ justifyContent: 'space-around'
+},
+c111: {
+ alignItems: 'center'
+},
+vl1: {
+ width: 1,
+ height: 50,
+ backgroundColor: 'black'
+},
+description: {
+ fontSize: 15,
+ marginVertical: 10,
+ width: '100%',
+ padding: 10,
+ paddingVertical: 20
+},
+postPic: {
+ width: '32%',
+ height: 120,
+ margin: 1
+},
+c13: {
+ flexDirection: 'row',
+ flexWrap: 'wrap',
+ marginBottom: 20,
+ justifyContent: 'center'
+},
+txt3: {
+ color: 'black',
+ //backgroundColor: 'green',
+ fontSize: 20
+},
+c2: {
+ width: '100%',
+ alignItems: 'center',
+ justifyContent: 'center',
+ height: 200
 }
-
-
-export default Signup_AccountCreated
-
-
-const styles = StyleSheet.create({
-
-})
+}
