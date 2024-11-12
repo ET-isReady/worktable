e6a241915c0051ce9b073a4ddf371210937bd101
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-12 14:33:39 -0700

    adding form.css file

--- uploadCode.js
+++ uploadCode.js
@@ -1,243 +1,110 @@
 import { Dimensions } from 'react-native'
 
 
+
+
 module.exports = {
- newThread: {
-   width: '100%',
-   flexDirection: 'row',
-   alignItems: 'center',
-   padding: 5
- },
- threadText: {
-   fontSize: 20,
-   marginLeft: 10
- },
- tinyProfPic: {
-   width: 30,
-   height: 30,
-   borderRadius: 44 / 2,
-   borderColor: '#00FF7F',
-   borderWidth: 1
- },
- postUsername: {
-   marginLeft: 10,
-   fontSize: 15,
-   fontWeight: 'bold',
-   color: '#000'
- },
- postNft_image: {
-   width: '100%',
-   height: 300,
-   borderRadius: 10,
- },
- userLists: {
-   width: '100%',
-   marginTop: 20
- },
- searchUsersCard: {
-   width: '97.5%',
-   height: 75,
-   backgroundColor: 'white',
-   borderRadius: 10,
-   marginVertical: 3,
-   flexDirection: 'row',
-   marginLeft: 5
- },
- searchProfilePic: {
-   width: 50,
-   height: 50,
-   borderRadius: 50,
-   borderColor: 'red',
-   borderWidth: 1,
-   marginTop: 5
- },
- searchView: {
-   marginLeft: 10
- },
- searchUsername: {
-   color: 'black',
-   fontSize: 20
+formTop: {
+  padding: 40
 },
-searchUserheader: {
- fontSize: 10,
- paddingRight: 49
+formTextLinkCenter: {
+  color: 'gray',
+  fontSize: 16
 },
-searchFans: {
- fontSize: 10,
- paddingTop: 2,
- color: '#0c6df5'
+searchFormHead: {
+  color: '#4632A1',
+  fontSize: 30,
+  textAlign: 'center'
 },
-containerFull: {
-  flex: 1,
-  backgroundColor: '#ffffff'
+signUp: {
+  color: 'red',
+  fontStyle: 'italic'
 },
-mainpageFull: {
-  flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center'
-},
-greenBackground: {
-  height: Dimensions.get('window').height / 2.5
-},
-brandView: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center'
-},
-brandViewText: {
-  color: '#ffffff',
-  fontSize: 40,
-  textTransform: 'lowercase'
-},
-logo1: {
-  height: 100,
-  resizeMode: 'contain'
-},
-bottomView: {
-  flex: 1.5,
-  backgroundColor: '#ffffff',
-  bottom: 50,
-  borderTopStartRadius: 60,
-  borderTopEndRadius: 60
-},
-goback: {
-  flexDirection: 'row',
-  position: 'absolute',
-  top: 30,
-  left: 20,
-  alignItems: 'center'
-},
-row: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  marginVertical: 10
-},
-logo2: {
-  width: 100,
-  height: 30,
-  resizeMode: 'contain',
-  marginLeft: -15
-},
-icons1: {
-  color: 'white',
-  fontSize: 35,
-},
-searchBar: {
-  width: '90%',
+formInput: {
+  borderColor: '#4632A1',
   backgroundColor: 'white',
-  borderRadius: 30,
-  paddingVertical: 10,
-  paddingHorizontal: 20,
-  marginTop: 10,
+  borderRadius: 10,
   fontSize: 18,
-  alignSelf: 'center'
+  borderBottomColor: '#000',
+  borderBottomWidth: .5,
+  marginVertical: -20,
+  paddingHorizontal: 15,
+  paddingVertical: 10,
+  marginTop: 5
 },
-noteCard: {
- backgroundColor: 'white',
- width: '100%',
- marginTop: 10,
- borderRadius: 20,
- padding: 10,
- flexDirection: 'row',
- borderRadius: 20,
- borderColor: '#B700B7',
- borderWidth: .5,
+formInputOld: {
+  borderColor: '#4632A1',
+  backgroundColor: 'white',
+  borderRadius: 10,
+  fontSize: 18,
+  borderBottomColor: '#000',
+  borderBottomWidth: .5,
+  marginVertical: 20,
+  paddingHorizontal: 15,
+  paddingVertical: 10,
+  marginTop: 5
 },
-noteProfileImage: {
- width: 50,
- height: 50,
- borderRadius: 50,
- borderColor: 'red',
- borderWidth: 1
+changePasswordLink: {
+  height: 50,
+  marginBottom: -60,
+  marginTop: 40,
+  flexDirection: 'row',
+  marginLeft: 195,
+  color: 'red'
 },
-noteC1: {
- marginLeft: 10
+formTextLinkRight: {
+  height: 50,
+  marginTop: 10,
+  flexDirection: 'row',
+  marginLeft: 195,
+  color: '#8f9195'
 },
-noteUsername: {
- fontSize: 15,
+formbtn: {
+  width: Dimensions.get('window').width / 2.5,
+  alignSelf: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#de813e',
+  borderRadius: 25,
+  borderColor: 'white',
+  borderWidth: 1,
+  fontSize: 20,
+  color: 'white',
+  textAlign: 'center',
+  paddingVertical: 10,
+  marginTop: 50
 },
-noteLastMessage: {
- fontSize: 19
+formHead2: {
+  fontSize: 20,
+  color: 'green',
+  textAlign: 'center',
 },
-my_img: {
-  width: 33,
-  height: 33,
-  resizeMode: 'contain',
-  borderRadius: 400/2,
-  borderColor: '#00FF7F',
-  borderWidth: 1
+formHead3: {
+  color: '#4632A1',
+  textAlign: 'center',
+  fontSize: 16
 },
-c1: {
- width: '100%',
- alignItems: 'center'
+formHead4: {
+  color: '#4632A1',
+  fontSize: 20,
+  textAlign: 'center'
 },
-profilePic: {
- width: 150,
- height: 150,
- borderRadius: 400/2,
- borderColor: '#00FF7F',
- borderWidth: 1,
- margin: 20
+formHead5: {
+  color: 'green',
+  textAlign: 'center',
+  fontSize: 16
 },
-txt: {
- color: 'black',
- //backgroundColor: 'green',
- fontSize: 20,
- fontWeight: 'bold',
- marginTop: -15,
- marginBottom: 10,
- paddingVertical: 10,
- paddingHorizontal: 20,
- borderRadius: 20
-},
-txt1: {
- color: 'green',
- fontSize: 15,
-},
-txt2: {
- color: 'green',
- fontSize: 20
-},
-c11: {
- width: '100%',
- flexDirection: 'row',
- justifyContent: 'space-around'
-},
-c111: {
- alignItems: 'center'
-},
-vl1: {
- width: 1,
- height: 50,
- backgroundColor: 'black'
-},
-description: {
- fontSize: 15,
- marginVertical: 10,
- width: '100%',
- padding: 10,
- paddingVertical: 20
-},
-postPic: {
- width: '32%',
- height: 120,
- margin: 1
-},
-c13: {
- flexDirection: 'row',
- flexWrap: 'wrap',
- marginBottom: 20,
- justifyContent: 'center'
-},
-txt3: {
- color: 'black',
- //backgroundColor: 'green',
- fontSize: 20
-},
-c2: {
- width: '100%',
- alignItems: 'center',
- justifyContent: 'center',
- height: 200
+formbtn2: {
+  width: Dimensions.get('window').width / 2.5,
+  alignSelf: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#4632A1',
+  borderRadius: 25,
+  borderColor: 'white',
+  borderWidth: 1,
+  fontSize: 20,
+  color: 'white',
+  textAlign: 'center',
+  paddingVertical: 10,
+  marginTop: 50
 }
 }
