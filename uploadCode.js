4294224ee280cac7986c83fc005d113191f3e7f6
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-12 17:25:25 -0700

    Add Post Big Card code

--- uploadCode.js
+++ uploadCode.js
@@ -1,125 +1,185 @@
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Image, Alert } from 'react-native'
-import React, { useState, useEffect } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
+import React, { useState } from 'react'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import Antdesign from 'react-native-vector-icons/AntDesign'
-import Entypo from 'react-native-vector-icons/Entypo'
-import { logo2, my_img } from '../CommonCss/pagecss'
-import logo from '../../assets/pp_logo.png'
-import img from '../../assets/profiledefault.jpg'
 
-
-const TopNavbar = ({ navigation, page })=>{
-
- const [userdata, setUserdata] = React.useState(null)
- const loaddata = async () => {
-   AsyncStorage.getItem('user')
-       .then(async (value) => {
-           fetch('http://10.0.2.2:3000/userdata', {
-               method: 'POST',
-               headers: {
-                   'Content-Type': 'application/json',
-                   'Authorization': 'Bearer ' + JSON.parse(value).token
-               },
-               body: JSON.stringify({ email: JSON.parse(value).user.email })
-           })
-               .then(res => res.json()).then(data => {
-                   if (data.message == 'User Found') {
-                       setUserdata(data.user)
-                   }
-                   else {
-                       Alert.alert('Login Again')
-                       navigation.navigate('Login')
-                   }
-               })
-               .catch(err => {
-                   navigation.navigate('Login')
-               })
-       })
-       .catch(err => {
-           navigation.navigate('Login')
-       })
+const Post_Big_Card = (
+{
+username,
+nft_image,
+profile_pic,
+likes,
+title,
 }
-useEffect(() => {
-   loaddata()
-}, [])
+)=>{
 
-return (
+//console.log(comments)
+
+const [isLiked, setIsLiked] = useState(false)
+const [showTitle, setShowTitle] = useState(false)
+
+return(
   <View style={styles.container}>
-  <TouchableOpacity onPress={()=>navigation.navigate('My_UserProfile')}>
-  
-  {
-    userdata?.profilepic.length > 0 ?
-    <Image style={styles.profilePic} source={{ uri: userdata.profilepic }} />
-      :
-      <Image source={img} style={my_img}/>
-
+    <View style={styles.c1}>
+    <Image source={{ uri: profile_pic }} style={styles.profilepic} />
+      <Text style={styles.username}>{username}</Text>
+    </View>
+    <Image source={{ uri: nft_image }} style={styles.nft_image}/>
+    <View style={styles.cardBottom}>
+    {
+      isLiked ?
+      <View style={styles.isLiked}>
+      <MaterialCommunityIcons name="trophy" size={26} color="#FFD700" onPress={()=>{
+        setIsLiked(false)
+      }}/>
+      <Text style={styles.likeCount}>{likes.length + 1}</Text>
+      <View style={styles.s22}>
+        {/*<MaterialCommunityIcons name="target-account" size={24} color="purple" onPress={
+          ()=>{
+            setShowTitle(!showTitle)
+          }
+        } />*/}
+        <FontAwesome name="bitcoin" size={24} color="#329932" onPress={
+          ()=>{
+            setShowTitle(!showTitle)
+          }
+        } />
+        {
+          showTitle == true &&
+          <View style={styles.title}>
+            {
+              title.map((item, index)=>{
+                return(
+                  <View style={styles.movement} key={item.id}>
+                    <Text>Title: {item.title}</Text>
+                    <Text>Desc: {item.desc}</Text>
+                  </View>
+                )
+              })
+            }
+          </View>
+        }
+      </View>
+      </View>
+       :
+      <View style={styles.notLiked}>
+      <MaterialCommunityIcons style={{paddingVertical: 5}} name="trophy-broken" size={20} color="#D4aF37" onPress={()=>{
+        setIsLiked(true)
+      }} />
+      {/*<Text style={styles.likeCount}>{likes.length}</Text>*/}
+      <View style={styles.s22}>
+        {/*<MaterialCommunityIcons name="target-account" size={24} color="purple"
+        onPress={
+          ()=>{
+            setShowTitle(!showTitle)
+          }
+        } />*/}
+        <FontAwesome name="bitcoin" size={24} color="#329932" onPress={
+          ()=>{
+            setShowTitle(!showTitle)
+          }
+        }/>
+        {
+          showTitle == true &&
+          <View style={styles.title}>
+            {
+              title.map((item, index)=>{
+                return(
+                  <View style={styles.movement} key={item.id}>
+                  <Text style={{padding: 2}}>NFT Info</Text>
+                    <Text>Title: {item.title}</Text>
+                    <Text>Desc: {item.desc}</Text>
+                  </View>
+                )
+              })
+            }
+          </View>
+        }
+      </View>
+      </View>
     }
-   {/* <Image source={img} style={my_img}/>    */}
-  </TouchableOpacity>
-
-  <TouchableOpacity onPress={()=>navigation.navigate('MainPage')}>
-  <Image source={logo} style={logo2}/>
-  </TouchableOpacity>
-
-  {
-    page === 'MainPage' &&
-    <Ionicons name="chatbubble-ellipses-outline" size={24} color="black"
-      onPress={
-        ()=>navigation.navigate('All_Chats')
-  } />
-}
-
-
-  {
-    page === 'My_UserProfile' &&
-     <FontAwesome name="dashboard" size={24} color="#de813e"
-      onPress={
-        ()=>navigation.navigate('Settings')
-  }
-
-    />
-
-  }
+    </View>
   </View>
-
 )
 }
 
-export default TopNavbar
+
+export default Post_Big_Card
 
 const styles = StyleSheet.create({
 container: {
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  width: '100%',
-  paddingVertical: 10,
-  padding: 10,
-  position: 'absolute',
-  top: 0,
-  zIndex: 100,
-  borderColor: '#00FF7F',
-  borderWidth: .5,
-  borderBottomRightRadius: 5,
-  borderBottomLeftRadius: 5,
-  backgroundColor: '#fff',
-  shadowColor: 'green',
-  shadowOffset: {
-    width: 0,
-    height: 1
-  },
-  shadowOpacity: .2,
-  shadowRadius: 2,
-  elevation: 20
+  width: "100%",
+  height: 580,
+  backgroundColor: 'white',
+  borderRadius: 10,
+  marginVertical: 10,
+  padding: 10
 },
-profilePic:{
-   width: 30,
-   height: 30,
-   borderRadius: 44/2,
-   borderColor: '#00FF7F',
-   borderWidth: 1
+c1: {
+  width: '100%',
+  flexDirection: 'row',
+  alignItems: 'center',
+  padding: 5
+},
+profilepic: {
+  width: 30,
+  height: 30,
+  borderRadius: 44/2,
+  borderColor: 'red',
+  borderWidth: 1
+},
+username: {
+  marginLeft: 10,
+  fontSize: 15,
+  fontWeight: 'bold'
+},
+nft_image: {
+  width: '100%',
+  height: '88%',
+  //aspectRatio: 1,
+  borderRadius: 10
+},
+cardBottom: {
+  width: '100%',
+  flexDirection: 'row',
+  alignItems: 'center'
+},
+notLiked: {
+  marginTop: 3,
+  flexDirection: 'row'
+},
+isLiked: {
+  width: '100%',
+  flexDirection: 'row'
+},
+likeCount: {
+  marginLeft: 5,
+  color: 'green',
+  fontWeight: 'bold',
+  fontSize: 16
+},
+s22: {
+  marginLeft: 20,
+  flexDirection: 'row'
+},
+title: {
+  width: '100%',
+  marginLeft: 10,
+  flexDirection: 'row',
+  paddingVertical: 5
+},
+movement: {
+  zIndex: 100,
+  width: '70%',
+  //marginTop: -50,
+  backgroundColor: '#fff',
+  borderRadius: 5,
+  padding: 10,
+  marginVertical: -100,
+  position: 'absolute'
 }
+
+
 })
