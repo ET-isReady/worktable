744e4e4a752c1f7d5850c74f5a6f25baacf4716e
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-13 12:34:25 -0700

    Add auth routes

--- uploadCode.js
+++ uploadCode.js
@@ -1,58 +1,210 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profilepic: {
-        type: String,
-        default: '',
-        // required: true
-    },
-    posts: {
-        type: Array,
-        default: []
-    },
-    followers: {
-        type: Array,
-        default: []
-    },
-    following: {
-        type: Array,
-        default: []
-    },
-    description: {
-        type: String,
-        default: ''
-    },
-    allmessages: {
-        type: Array,
-        default: []
-    }
+require('dotenv').config()
+
+
+const nodemailer = require('nodemailer')
+
+
+async function mailer(receiveremail, code) {
+   //console.log("Mailer function called.")
+
+
+
+
+   let transporter = nodemailer.createTransport({
+       host: "smtp.gmail.com",
+       port: 587,
+       secure: false, // true for 465, false for other ports
+       requireTLS: true,
+       auth: {
+           user: process.env.NodeMailer_email, // generated ethereal user
+           pass: process.env.NodeMailer_password, // generated ethereal password
+       },
+   })
+
+
+
+
+   let info = await transporter.sendMail({
+       from: "Poetionpics",
+       to: `${receiveremail}`,
+       subject: "Poetionpics Email Verfication",
+       text: `Greetings! Your Poetionpics Verification Code is ${code}`,
+       html: `Greetings! Your Poetionpics Verification Code is <b>${code}</b>`
+   })
+   console.log("Message Sent: %s", info.messageId)
+   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+}
+
+
+
+
+router.post('/verify', (req, res) => {
+   console.log('sent by client', req.body)
+   const { email } = req.body
+
+
+
+
+   if (!email) {
+       return res.status(422).json({ error: "Please add all the fields" })
+   }
+   User.findOne({ email: email })
+       .then(async (savedUser) => {
+           if (savedUser) {
+               return res.status(422).json({ error: "Invalid Credentials" })
+           }
+           try {
+               let VerificationCode = Math.floor(100000 + Math.random() * 900000)
+               await mailer(email, VerificationCode)
+               console.log("Verification Code", VerificationCode)
+               res.send({ message: "Verification Code Sent to your Email", VerificationCode, email })
+           }
+           catch (err) {
+               console.log(err)
+           }
+       }
+       )
 })
 
 
-userSchema.pre('save', async function (next) {
-    const user = this;
-    console.log("Just before saving before hashing  ", user.password);
-    if (!user.isModified('password')) {
-        return next();
-    }
-    user.password = await bcrypt.hash(user.password, 8);
-    console.log("Just before saving after hashing  ", user.password);
-    next();
+router.post('/changeusername', (req, res)=>{
+   const { username, email } = req.body
+
+
+   User.find({ username }).then( async (savedUser) =>{
+       if(savedUser.length > 0){
+           return res.status(422).json({ error: "Username already exists"})
+       } else{
+           return res.status(200).json({ message: "Username Available!", username, email })
+       }
+   })
 })
 
-mongoose.model("User", userSchema);
+
+router.post('/signup', async (req, res)=>{
+   const { username, email, password } = req.body
+
+
+   if( !username || !email || !password ){
+       return res.status(422).json({ error: "Please add all the fields" })
+   }else{
+       const user = new User({
+           username,
+           email,
+           password
+       })
+       try{
+           await user.save()
+           const token = jwt.sign({ _id : user._id }, process.env.JWT_SECRET)
+           return res.status(200).json({ message: "You Have Been Registered Successfully!", token })
+       }
+       catch(err){
+           console.log(err)
+           return res.status(422).json({ error: "User Not Registered"})
+       }
+   }
+})
+
+
+//forgot password
+router.post('/verifyfp', (req, res) => {
+   console.log('sent by client', req.body)
+   const { email } = req.body
+
+
+   if (!email) {
+       return res.status(422).json({ error: "Please add all the fields" })
+   }
+   User.findOne({ email: email })
+       .then(async (savedUser) => {
+           if (savedUser) {
+               try {
+                   let VerificationCode = Math.floor(100000 + Math.random() * 900000)
+                   await mailer(email, VerificationCode)
+                   console.log("Verification Code", VerificationCode)
+                   res.send({ message: "Verification Code Sent to your Email", VerificationCode, email })
+               }
+               catch (err) {
+                   console.log(err)
+               }
+           } else{
+               return res.status(422).json({ error: "Invalid Credentials" })
+           }
+       }
+       )
+})
+
+
+router.post('/resetpassword', (req, res)=>{
+   const { email, password } = req.body
+
+
+   if( !email || !password ){
+       return res.status(422).json({ error: "Please add all the fields"})
+   } else{
+       User.findOne({ email: email })
+       .then( async (savedUser)=>{
+           if(savedUser){
+               savedUser.password = password
+               savedUser.save()
+               .then(user =>{
+                   res.json({ message: "Password Changed Successfully!"})
+               })
+               .catch(err =>{
+                   console.log(err)
+               })
+           }
+           else{
+               return res.status(422).json({ error: "Invalid Credentials"})
+           }
+       })
+   }
+})
+
+
+router.post('/login', (req, res) =>{
+   const { email, password } = req.body
+   if(!email || !password){
+     return res.status(422).json({ error: "Please add all the fields."})
+   } else{
+     User.findOne({ email: email})
+     .then(savedUser =>{
+       if(!savedUser){
+         return res.status(422).json({ error: "Invalid Credentials. Please Try Again."})
+       } else{
+         console.log(savedUser)
+         bcrypt.compare(password, savedUser.password)
+         .then(
+           doMatch => {
+             if(doMatch){
+               const token = jwt.sign({_id: savedUser._id}, process.env.JWT_SECRET)
+ 
+ 
+               const {_id, username, email} = savedUser
+ 
+ 
+               res.json({ message: "Successfully Signed In!", token, user:{_id, username, email}})
+             } else{
+               return res.status(422).json({ error: "Invalid Credentials"})
+             }
+           }
+         )
+         //res.status(200).json({ message: "User Logged In Successfully!", savedUser})
+       }
+     })
+     .catch(err =>{
+       console.log(err)
+     })
+   }
+  })
+
+
+module.exports = router
