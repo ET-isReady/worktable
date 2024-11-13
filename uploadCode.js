071f6c69f5836c8e2d59b208ef2d902c4a91fc2a
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-12 17:15:13 -0700

    Add Bottom Nav Bar Code

--- uploadCode.js
+++ uploadCode.js
@@ -1,65 +1,86 @@
-import { StyleSheet, Text, View, StatusBar, Alert } from 'react-native'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontAwesom from 'react-native-vector-icons/FontAwesome'
+import Antdesign from 'react-native-vector-icons/AntDesign'
+import Entypo from 'react-native-vector-icons/Entypo'
+import { icons1 } from '../CommonCss/pagecss'
 
-import BottomNavbar from '../../components/BottomNavbar'
-import TopNavbar from '../../components/TopNavbar'
-import FollowersRandomPost from '../../components/FollowersRandomPost'
-import AntDesign from 'react-native-vector-icons/AntDesign'
+  const BottomNavbar = ({navigation, page}: Props) => {
 
-const MainPage = ({ navigation })=>{
-
-
-const [ userdata, setUserdata ] = useState(null)
-
-
-useEffect(()=>{
-  AsyncStorage.getItem('user')
-  .then(data => {
-    setUserdata(JSON.parse(data))
-  })
-  .catch(err => Alert.alert(err))
-
-
-}, [])
-console.log('userdata ', userdata)
-
+// console.log(page)
 return (
   <View style={styles.container}>
   <StatusBar />
-  <TopNavbar navigation = {navigation} page={"MainPage"} />
-  <AntDesign name="pluscircle" size={50} color="#FF80ED"
-  style={styles.pluscircle} onPress={()=>navigation.navigate('AddPost')} />
-  <BottomNavbar navigation = {navigation} page={'MainPage'} />
-  <FollowersRandomPost />
+
+  {
+    page === 'NotificationPage' ?
+   
+    <MaterialCommunityIcons name="webhook" color="#065535" style={styles.activeicons1}
+        onPress={()=>navigation.navigate('NotificationPage')}
+   
+    />
+    :
+    <MaterialCommunityIcons name="webhook" style={icons1}
+        onPress={()=>navigation.navigate('NotificationPage')}
+    
+    />
+  
+  }
+
+
+  {/*}<FontAwesome name="bitcoin" style={icons1} />*/}
+
+
+  {
+    page === 'MainPage' ?
+    <Entypo name="upload-to-cloud" color="#065535"  style={styles.activeicons1}
+      onPress={()=>navigation.navigate('MainPage')}
+    />
+    :
+    <Entypo name="upload-to-cloud"  style={icons1}
+      onPress={()=>navigation.navigate('MainPage')}
+    />
+  }
+
+
+  {
+    page === 'SearchUserPage' ?
+    <MaterialCommunityIcons name="cloud-search" color="#065535" style={styles.activeicons1}
+      onPress={()=>navigation.navigate('SearchUserPage')}
+    />
+    :
+    <MaterialCommunityIcons name="cloud-search" style={icons1}
+      onPress={()=>navigation.navigate('SearchUserPage')}
+    />
+  }
+
   </View>
 )
 }
 
-export default MainPage
+export default BottomNavbar
 
 const styles = StyleSheet.create({
 container: {
-  width: '100%',
-  height: '100%',
-  paddingVertical: 50
-},
-pluscircle: {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-evenly',
+  borderTopLeftRadius: 20,
+  borderTopRightRadius: 20,
   position: 'absolute',
+  bottom: 0,
+  width: '100%',
   zIndex: 100,
-  bottom: 50,
-  right: 10,
-  borderColor: '#00FF7F',
-  borderWidth: .5,
-  borderRadius: 25,
-  backgroundColor: '#fff',
-  shadowColor: 'green',
-  shadowOffset: {
-    width: 0,
-    height: 1
-  },
-  shadowOpacity: .2,
-  shadowRadius: 2,
-  elevation: 20
+  paddingVertical: 5,
+  backgroundColor: '#00B900',
+  opacity: .70,
+  alignItems: 'center'
+},
+activeicons1: {
+  backgroundColor: '#E5E5E5',
+  borderRadius: 50,
+  fontSize: 35,
+  paddingHorizontal: 15
 }
 })
