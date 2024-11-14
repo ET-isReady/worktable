722a8faa63291d15b21d2b815c718a31089f58ff
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-13 20:34:57 -0700

    Add Change Username code

--- uploadCode.js
+++ uploadCode.js
@@ -6,66 +6,67 @@ import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, 
     import greenBg from '../../../assets/nft03.jpeg'
     import { containerFull, logo1, greenBackground,
       brandView, brandViewText, bottomView, goback } from '../../CommonCss/pagecss'
-      import Ionicons from 'react-native-vector-icons/Ionicons'
+    import Ionicons from 'react-native-vector-icons/Ionicons'
     import { formTop, formTextLinkCenter, formHead, formHead2, signUp, formInput, formTextLinkRight, formbtn,
       formHead3, formHead5 } from '../../CommonCss/formcss'
-
-    const ChangeUserHeader = ({ navigation })=>{
+ 
+    const ChangeUsername = ({ navigation })=>{
   
-  
-      const [userheader, setUserHeader ] = useState('')
+      const [username, setUsername ] = useState('')
       const [loading, setLoading] = useState(false)
+      const [ userdata, setUserdata ] = useState(null)
   
-      const handleUserHeader = () => {
-          if ( userheader == '') {
-              Alert.alert('Please Enter Your Header')
+      const handleUsername = () => {
+          if ( username == '') {
+              Alert.alert('Please enter username')
           }
           else {
               setLoading(true)
-              AsyncStorage.getItem('user').then(
-                    data => {
-                        fetch('http://10.0.2.2:3000/setuserheader', {
-                            method: 'post',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                email: JSON.parse(data).user.email,
-                                userheader: userheader
-                            })
-                        })
-                            .then(res => res.json())
-                            .then(data => {
-                                if (data.message === "Profile Header Updated!") {
-                                    setLoading(false)
-                                    Alert.alert('Your info has been updated!')
-                                    navigation.navigate('MainPage')
-                                }
-                                else if (data.error === "Invalid Credentials") {
-                                    Alert.alert('Invalid Credentials')
-                                    setLoading(false)
-                                    navigation.navigate('Login')
-                                }
-                                else {
-                                    setLoading(false)
-                                    Alert.alert("Please Try Again");
-                                }
-                            })
-                            .catch(err => {
-                                Alert.alert('Something went wrong')
-                                setLoading(false)
-                            })
-                    }
-                )
-                    .catch(err => {
-                        Alert.alert('Something went wrong')
-                        setLoading(false)
-                    })
-            }
+              AsyncStorage.getItem('user')
+                  .then(data => {
+                      fetch('http://10.0.2.2:3000/setusername', {
+                          method: 'post',
+                          headers: {
+                              'Content-Type': 'application/json'
+                          },
+                          body: JSON.stringify({
+                              email: JSON.parse(data).user.email,
+                              username: username
+                          })
+                      })
+                          .then(res => res.json())
+                          .then(
+                              data => {
+                                  if (data.message === "Username Updated!") {
+                                      setLoading(false)
+                                      Alert.alert('Username Successfully Updated!')
+                                      navigation.navigate('MainPage')
+                                  }
+                                  else if (data.error === "Invalid Credentials") {
+                                      Alert.alert('Invalid Credentials')
+                                      setLoading(false)
+                                      navigation.navigate('Login')
+                                  }
+                                  else {
+                                      setLoading(false)
+                                      Alert.alert("Username not available");
+                                  }
+                              }
+                          )
+                          .catch(err => {
+                              Alert.alert('Something went wrong')
+                              setLoading(false)
+                          })
+                  })
+                  .catch(err => {
+                      Alert.alert('Something went wrong')
+                      setLoading(false)
+                  }
+                  )
+          }
   
-            // navigation.navigate('Signup_ChoosePassword')
-        }
-       
+          // navigation.navigate('Signup_ChoosePassword')
+      }
       return (
   
         <ScrollView style={containerFull}>
@@ -86,19 +87,17 @@ import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, 
         <View style={bottomView}>
         <View style={formTop}>
   
-        <Text style={formHead5}>Update Your General Information</Text>
+        <Text style={formHead5}>Update Your Username</Text>
           <View style={{ marginTop: 50 }}>
-            <TextInput placeholder='Enter New Info' style={formInput}
-            onChangeText={(text)=>setUserHeader(text)}
-            multiline={true}
-            numberOfLines={4}
+            <TextInput placeholder='Enter New Username' style={formInput}
+            onChangeText={(text)=>setUsername(text)}
             />
           </View>
           {
-            loading ? <ActivityIndicator />
+            loading ? <ActivityIndicator size="large" color='#00FF7F' />
             :
             <Text style={formbtn}
-              onPress={()=> handleUserHeader()}>
+              onPress={()=> handleUsername()}>
               Save
             </Text>
           }
@@ -108,7 +107,7 @@ import { StyleSheet, Text, View, Image, TextInput, ImageBackground, ScrollView, 
       )
     }
   
-    export default ChangeUserHeader
+    export default ChangeUsername
   
     const styles = StyleSheet.create({
   
