938719afc223e972e93194fd6b16ba3d1c6e0625
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-14 12:34:28 -0700

    Add Search User code

--- uploadCode.js
+++ uploadCode.js
@@ -1,49 +1,94 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native'
-import React from 'react'
-import BottomNavbar from '../../components/BottomNavbar'
-import TopNavbar from '../../components/TopNavbar'
+import { StyleSheet, Text, View, StatusBar, TextInput, ScrollView, ActivityIndicator } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { containerFull, logo1, hr80, mainpageFull, searchBar } from '../../CommonCss/pagecss'
+import BottomNavbar from '../../Components/BottomNavbar'
+import TopNavbar from '../../Components/TopNavbar'
+import UsersCard from '../../Cards/UsersCard'
 
 
-type Props = {
-   navigation: any
- };
+const SearchUserPage = ({ navigation }) => {
 
 
-const NotificationPage = ({ navigation }: Props)=>{
+  const [keyword, setKeyword] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([])
+  const [error, setError] = useState(null)
+
+  const getallusers = async () => {
+      if (keyword.length > 0) {
+          setLoading(true)
+          fetch('http://10.0.2.2:3000/searchuser', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ keyword: keyword })
+          })
+              .then(res => res.json())
+              .then(data => {
+                
+                  if (data.error) {
+                      setData([])
+                      setError(data.error)
+                      setLoading(false)
+                  }
+                  else if (data.message == 'User Found') {
+                      setError(null)
+                      setData(data.user)
+                      setLoading(false)
+                  }
+              })
+              .catch(err => {
+                  setData([])
+                  setLoading(false)
+              })
+      }
+      else {
+          setData([])
+          setError(null)
+      }
+  }
+
+  useEffect(() => {
+      getallusers()
+  }, [keyword])
+
 return (
   <View style={styles.container}>
   <StatusBar />
   <TopNavbar navigation = {navigation} />
-  <BottomNavbar navigation = {navigation} page={"NotificationPage"} />
-  <View style={styles.c1}>
-  <View style={styles.notification}>
-    <Text>Some Shoutouts</Text>
-  </View>
-  <View style={styles.notification}>
-    <Text>Some Shoutouts</Text>
-  </View>
-  <View style={styles.notification}>
-    <Text>Some Shoutouts</Text>
-  </View>
-  <View style={styles.notification}>
-    <Text>Some Shoutouts</Text>
-  </View>
-  </View>
+  <BottomNavbar navigation={navigation} page={"SearchUserPage"} />
+
+  <TextInput style={searchBar} placeholder="Search Stars or Sellers" onChangeText={(text)=>{
+      setKeyword(text)
+  }} />
+  {
+    loading ?
+    <ActivityIndicator size="large" color='#00FF7F' />
+    :
+    <>
+    {
+      error ?
+    <Text style={{ color: 'black' }}>{error}</Text>
+      :
+      //use code below for upper corner small profile image
+    <ScrollView style={styles.userLists}>
+    {
+      data.map((item, index)=>{
+        return <UsersCard key={item.username} user={item}
+        navigation={navigation}
+        />
+      })
+    }
+    </ScrollView>
+    }
+    </>
+  }
   </View>
 )
 }
 
-
-
-
-
-
-
-
-export default NotificationPage
-
-
-
+export default SearchUserPage
 
 const styles = StyleSheet.create({
 container: {
@@ -51,17 +96,11 @@ width: '100%',
 height: '100%',
 paddingVertical: 50
 },
-c1: {
+userLists: {
   width: '100%',
-  height: '100%',
-  alignItems: 'center'
-},
-notification: {
-  width: '98%',
-  height: 50,
-  backgroundColor: 'grey',
-  marginTop: 10,
+  marginTop: 20
 }
 })
+
 
    
