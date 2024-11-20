5fde1aa8395fb5e61795d32dfa8fdaec0c54f671
Eric Tenaya <jedihomeslice@gmail.com>
2024-11-19 17:42:30 -0700

    Add Platform URI

--- uploadCode.js
+++ uploadCode.js
@@ -1,61 +1,17 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, ActivityIndicator } from 'react-native'
-import React from 'react'
-import profiledefault from '../../assets/profiledefault.jpg'
+import { Platform } from 'react-native'
 
 
-const UsersCard = ({ user, navigation })=>{
-
-return(
-  //use code below for upper corner small profile image
-  <TouchableOpacity onPress={()=>{
-    navigation.navigate('Other_UserProfile', { user: user })
-  }}>
-    <View style={styles.ChatCard}>
-  {
-    user.profilepic
-    ?
-    <Image source={{ uri: user.profilepic }} style={styles.profile_image}/>
-    :
-    <Image source={ profiledefault } style={styles.profile_image}/>
-  }
+let URI = ''
 
 
-  <View style={styles.c1}>
-  <Text style={styles.username}>{user.username}</Text>
-  </View>
-  </View>
-  </TouchableOpacity>
-)
+if(Platform.OS === 'ios'){
+   URI = 'http://localhost:3000/api/v1'
+} else{
+   URI = 'http://10.0.2.2:3000/api/v1'
 }
 
-export default UsersCard
 
-const styles = StyleSheet.create({
-ChatCard: {
-  width: '100%',
-  height: 75,
-  backgroundColor: 'white',
-  borderRadius: 10,
-  marginVertical: 10,
-  flexDirection: 'row',
-  marginTop: 10,
-  padding: 10,
-},
-profile_image: {
-  width: 50,
-  height: 50,
-  borderRadius: 50,
-  borderColor: 'red',
-  borderWidth: 1
-},
-c1: {
-  marginLeft: 10
-},
-username: {
-  color: 'black',
-  fontSize: 20
-}
-})
+export { URI }
 
 
 
